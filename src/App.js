76855e27b42cc45e6488ba3d5acd8b781525c449
konalam venkatesh <konalamvenkatesh111@gmail.com>
2023-03-02 21:17:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { PATHS } from "./paths";
+import Login from "./components/Login/Login";
+import AddExpense from "./components/AddExpense/AddExpense";
+import ManageExpense from "./components/ManageExpense/ManageExpense";
+import PrivateRoutes from "./routes/PrivateRoutes";
+import RestrictedRoutes from "./routes/RestrictedRoutes";
+
+export const centeredStyle = {
+  width: "100%",
+  height: "100vh",
+  display: "grid",
+  placeItems: "center",
+  background: "dodgerblue",
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<PrivateRoutes />}>
+        <Route path={PATHS.ADD_EXPENSE} element={<AddExpense />} />
+        <Route path={PATHS.MANAGE_EXPENSE} element={<ManageExpense />} />
+      </Route>
+
+      <Route path="/" element={<RestrictedRoutes />}>
+        <Route path={PATHS.LOGIN} element={<Login />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, CardBody, Button, Text, Flex } from "@chakra-ui/react";
+import { PATHS } from "../../paths";
+
+const Nav = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Card mb={50}>
+        <CardBody>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Text color="dodgerblue" fontWeight="800" fontSize="2em">
+              Money Movement Tracker
+            </Text>
+            <Flex gap={5}>
+              <Button
+                // ml={80}
+                colorScheme="twitter"
+                onClick={() => {
+                  navigate(PATHS.ADD_EXPENSE);
+                }}
+              >
+                Add Expense
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  window.localStorage.clear();
+                  navigate(PATHS.LOGIN);
+                }}
+              >
+                Logout
+              </Button>
+            </Flex>
+          </Flex>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Nav;
